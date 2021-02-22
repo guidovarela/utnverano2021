87d36245bcb1143b2607c5e917be1877d8852914
Guido Varela Coletta <guido.varela@gmail.com>
2021-02-22 16:07:43 -0300

    clase 14

---
+++ 14-introJS/pruebas/js/scripts.js
@@ -0,0 +1,28 @@
+    //Javascript interno
+    /*comentario multilinea*/
+    
+    //Variables
+    // 1ero Definimos la variable
+    // 2do ejecutamos / operamos / utilizamos esa variable
+
+    //Estas variables son de tipo "locales" -> se utilizan en todo el programa
+
+    // var nombreVariable igual dato
+    var nombre = "Guido";
+    // alert(nombre) 
+    
+    var nombre = "Mabel"
+    console.log(nombre)
+    document.write(nombre)
+
+    //Tipos de datos
+
+    var cadena = "string o cadena de caracteres"
+    var numerica = 30
+    var logica = false //booleana
+    var dobleNumero = numerica
+
+    console.log(numerica)
+    console.log(dobleNumero)
+
+    alert("Ahora viene: " + actividad)
