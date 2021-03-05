17ecf49211609a6627c953192d4193e5d2674497
Guido Varela Coletta <guido.varela@gmail.com>
2021-03-05 14:39:13 -0300

    clase 19

--- 17-js/ejercicioArrays/ejArrays.js
+++ 17-js/ejercicioArrays/ejArrays.js
@@ -7,14 +7,21 @@ let position = 1
 for (let i = 0; i < clasificacion.length; i++) {
     document.write(`${position++}º - ${clasificacion[i]}  `)
 }
+//ES6
+for (const ciclo of clasificacion) {
+    document.write(`${position++}º - ${ciclo}  `)
+}
 
 //Cambios de clasificación
 clasificacion[2] = "Clara";
 clasificacion[3] = "Mariano";
+var clasificacion = ["Ana", "Pedro", "Clara", "Mariano", "Axel", "Anton"];
+
 clasificacion.pop();
 clasificacion.splice(1, 0, "Roberto", "Analia");
+
 clasificacion.unshift("Marcela");
- 
+
 //clasificación Final
 document.write("<hr>");
 position=1
